470ad5c19aa6ccee5e19ede75b9b18d3ffc194ed
Jon Duell <duellj@gmail.com>
2011-01-05 00:38:39 +0000

    #1011312 by Chris Gillis: Removes notices by setting option defaults Changes theme_datatables to use drupal_add_library

--- views/datatables-view.tpl.php
+++ views/datatables-view.tpl.php
@@ -7,14 +7,14 @@
  * - $title : The title of this group of rows.  May be empty.
  * - $header: An array of header labels keyed by field id.
  * - $fields: An array of CSS IDs to use for each field id.
- * - $class: A class or classes to apply to the table, based on settings.
+ * - $classes: A class or classes to apply to the table, based on settings.
  * - $row_classes: An array of classes to apply to each row, indexed by row
  *   number. This matches the index in $rows.
  * - $rows: An array of row items. Each row is an array of content.
  *   $rows are keyed by row number, fields within rows are keyed by field ID.
  */
 ?>
-<table id="<?php print $id ?>" class="<?php print implode(' ', $class); ?>">
+<table id="<?php print $id ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php if (!empty($title)) : ?>
     <caption><?php print $title; ?></caption>
   <?php endif; ?>
